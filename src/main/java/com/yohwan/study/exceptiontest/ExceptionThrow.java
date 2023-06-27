b014b1482a51789d5f35c777eb3b6f6aa1d879a8
limyohwan <dyghks7102@naver.com>
2023-06-27 11:32:14 +0900

    feat. throw, throws 정리

---
+++ src/main/java/com/yohwan/study/exceptiontest/ExceptionThrow.java
@@ -0,0 +1,10 @@
+package com.yohwan.study.exceptiontest;
+
+public class ExceptionThrow {
+    public static void main(String[] args) {
+    }
+
+    public void throwException() throws Exception {
+        throw new Exception("error");
+    }
+}
