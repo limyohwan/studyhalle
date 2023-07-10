a95358dc8de3eee3deba2c7eaecd84d4781c99ee
limyohwan <dyghks7102@naver.com>
2023-07-10 11:07:01 +0900

    feat. custom 애노테이션 정의 및 사용법 추가

---
+++ src/main/java/com/yohwan/study/annotation/CustomAnnotation.java
@@ -0,0 +1,13 @@
+package com.yohwan.study.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface CustomAnnotation {
+    String value();
+    int count() default 1;
+}
---
+++ src/main/java/com/yohwan/study/annotation/CustomMain.java
@@ -0,0 +1,9 @@
+package com.yohwan.study.annotation;
+
+//@CustomAnnotation(value = "Custom", count = 1)
+@CustomAnnotation("Custom")
+public class CustomMain {
+    public static void main(String[] args) {
+
+    }
+}
