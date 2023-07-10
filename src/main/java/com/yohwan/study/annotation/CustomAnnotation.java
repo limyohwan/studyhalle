65132bdf338bed3b327519701abb87ba7414c4cf
limyohwan <dyghks7102@naver.com>
2023-07-10 14:32:42 +0900

    feat. @Documented 추가

--- src/main/java/com/yohwan/study/annotation/CustomAnnotation.java
+++ src/main/java/com/yohwan/study/annotation/CustomAnnotation.java
@@ -1,10 +1,8 @@
 package com.yohwan.study.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface CustomAnnotation {
