f2c5cb6c048c9a30ea57b7113bdb3be9e8ae63c0
limyohwan <dyghks7102@naver.com>
2023-07-13 17:20:26 +0900

    feat. BufferedInputStream 예제 추가

---
+++ src/main/java/com/yohwan/study/streamtest/BufferedInputStreamExample.java
@@ -0,0 +1,19 @@
+package com.yohwan.study.streamtest;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class BufferedInputStreamExample {
+    public static void main(String[] args) {
+        //BufferedInputStream의 기본 버퍼 사이즈 = 8192
+        try (BufferedInputStream stream = new BufferedInputStream(new FileInputStream("hello.txt"))) {
+            int data;
+            while ((data = stream.read()) != -1) {
+                System.out.print((char) data);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
