aaa5f243225ade2ff53f00e0c17763d427ac73c9
limyohwan <dyghks7102@naver.com>
2023-07-25 11:26:26 +0900

    feat. 스프링 빈 등록 예제

---
+++ src/main/java/com/yohwan/study/generictest/fruit/AppConfig.java
@@ -0,0 +1,18 @@
+package com.yohwan.study.generictest.fruit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    // 스프링 빈 등록 예제
+    @Bean
+    public GenericDao<Apple, Integer> appleDao() {
+        return new GenericDao<>();
+    }
+
+    @Bean
+    public GenericDao<Banana, Integer> bananaDao() {
+        return new GenericDao<>();
+    }
+}
