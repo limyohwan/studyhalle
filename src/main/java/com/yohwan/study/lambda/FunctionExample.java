44e7aa8a568fb4d779c6c04a3d86e85f0a303a20
Yohwan Lim <dyghks7102@naver.com>
2023-08-11 23:56:50 +0900

    refactor. ()괄호 통일

--- src/main/java/com/yohwan/study/lambda/FunctionExample.java
+++ src/main/java/com/yohwan/study/lambda/FunctionExample.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public class FunctionExample {
     public static void main(String[] args) {
-        Function<String, Integer> parseToInt = s -> Integer.parseInt(s); // (s) -> Integer.parseInt(s), Integer::parseInt
+        Function<String, Integer> parseToInt = (s) -> Integer.parseInt(s); // s -> Integer.parseInt(s), Integer::parseInt
         int result = parseToInt.apply("42");
 
         System.out.println("Parsed integer: " + result);
