cc53e62e03c9c442797a9393169ae7f3f62fe286
Yohwan Lim <dyghks7102@naver.com>
2023-08-12 23:24:15 +0900

    feat. 커스텀 함수형 인터페이스 정의하는법 추가

--- src/main/java/com/yohwan/study/lambda/FunctionalInterfaceExample.java
+++ src/main/java/com/yohwan/study/lambda/FunctionalInterfaceExample.java
@@ -4,8 +4,25 @@ import java.util.function.BinaryOperator;
 
 public class FunctionalInterfaceExample {
     public static void main(String[] args) {
+        binaryOperatorExample();
+
+        myFunctionExample(6, 3);
+    }
+
+    private static void binaryOperatorExample() {
         BinaryOperator<Integer> add = (a, b) -> a + b;
         int result = add.apply(3, 5);
         System.out.println(result);
+    }
+
+    private static void myFunctionExample(int x, int y) {
+        MyFunction add = (a, b) -> a + b;
+        MyFunction minus = (a, b) -> a - b;
+
+        int result1 = add.calculate(x, y);
+        int result2 = minus.calculate(x, y);
+
+        System.out.println("x + y : " + result1);
+        System.out.println("x - y " + result2);
     }
 }
---
+++ src/main/java/com/yohwan/study/lambda/MyFunction.java
@@ -0,0 +1,6 @@
+package com.yohwan.study.lambda;
+
+@FunctionalInterface
+public interface MyFunction {
+    int calculate(int a, int b);
+}
